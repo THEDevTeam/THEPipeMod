f1eeb826dac52bce1b3f891fa46605a72b5bfd8d
Rob4001 <robert.wilson1717@gmail.com>
2012-04-05 17:54:41 +0100

    Initial Commit and pom config

---
+++ src/main/java/com/thedevteam/thepipemod/ThePipeMod.java
@@ -0,0 +1,24 @@
+package com.thedevteam.thepipemod;
+
+import org.spout.api.plugin.CommonPlugin;
+
+/**
+ * 
+ *
+ */
+public class ThePipeMod extends CommonPlugin 
+{
+
+	@Override
+	public void onDisable() {
+		throw new UnsupportedOperationException("Not done yet!");
+		
+	}
+
+	@Override
+	public void onEnable() {
+		throw new UnsupportedOperationException("Not done yet!");
+		
+	}
+
+}
